1197e1ebe1a94a0d927a1d5652635030941e5508
Pedro Hernández <pedro@bunsan.io>
2019-05-03 11:57:43 -0500

    Initial commit

---
+++ app/src/main/java/io/keepcoding/filmica/Film.kt
@@ -0,0 +1,9 @@
+package io.keepcoding.filmica
+
+data class Film(
+        val title: String = "No title",
+        val genre: String = "No genre",
+        val rating: Float = 0.0f,
+        val overview: String = "No overview",
+        val date: String = "1999-09-19"
+)
